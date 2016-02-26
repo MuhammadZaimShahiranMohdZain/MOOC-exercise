36dc68fd97f77e107dda6b7894d0e806263932aa
MuhammadZaimShahiran <MuhammadZaimShahiran.MohdZain@sicpa.com>
2016-02-27 07:31:24 +0800

    26/2/16

--- week3/e68.java
+++ week3/e68.java
@@ -1,5 +1,38 @@
 package week3;
 
-public class e68 {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class e68 {
+	public static boolean moreThanOnce(ArrayList<Integer> list, int number) {
+	    // write your code here
+		int count = 0;
+		for(int i:list){
+			if(i==number){
+				count++;
+			}
+		}
+		if(count>1)
+			return true;
+		else
+			return false;
+	}
+
+	public static void main(String[] args) {
+	    Scanner reader = new Scanner(System.in);
+
+	    ArrayList<Integer> list = new ArrayList<Integer>();
+	    list.add(3);
+	    list.add(2);
+	    list.add(7);
+	    list.add(2);
+
+	    System.out.println("Type a number: ");
+	    int number = Integer.parseInt(reader.nextLine());
+	    if (moreThanOnce(list, number)) {
+	        System.out.println(number + " appears more than once.");
+	    } else {
+	        System.out.println(number + " does not appear more than once.");
+	    }
+	}
 }
---
+++ week4/e71.java
@@ -0,0 +1,36 @@
+package week4;
+
+import java.util.ArrayList;
+
+public class e71 {
+	public static void combine(ArrayList<Integer> first, ArrayList<Integer> second){
+		ArrayList<Integer> help = new ArrayList<>();
+		for(int i=0; i<first.size();i++)
+			for(int j:second){
+				if(j == first.get(i)){
+					System.out.println(first.get(i));
+					System.out.println(j);
+					break;
+				}
+			}
+		System.out.println(help);
+	}
+	public static void main(String[] args) {
+		ArrayList<Integer> list1 = new ArrayList<Integer>();
+		ArrayList<Integer> list2 = new ArrayList<Integer>();
+
+		list1.add(4);
+		list1.add(3);
+		list1.add(7);
+
+		list2.add(5);
+		list2.add(10);
+		list2.add(7);
+
+		combine(list1, list2);
+
+		System.out.println(list1); // prints [4, 3, 5, 10, 7]
+
+		System.out.println(list2); // prints [5, 10, 7]
+	}
+}
